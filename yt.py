f250a3cc4904a6ff6b46d7b9d134d547fbc93177
shouryaveer <shouryaveermehta98@gmail.com>
2020-11-21 12:07:48 +0530

    yt.py V2.0

--- yt.py
+++ yt.py
@@ -1,11 +1,9 @@
 from pytube import YouTube
 
-url = 'your-youtube-video-url'
-video = YouTube(url)
-
-print(video.streams.all())
-
+video_link = input("Enter YouTube link: ")
+yt = YouTube(video_link)
+print(yt.streams.filter(subtype='mp4'))
 # You will get a list of streams available for your video
 # Choose your favorable stream by their itag and mention the itag below
-
-video.streams.get_by_itag('your-itag').download('your-download-path')
+itag = int(input('Enter itag: '))
+yt.streams.get_by_itag(itag).download('D:\\')
